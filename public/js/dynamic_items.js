42c5c3a88744a002a65b80d1b7548ef177d218fc
asim7056 <asim@einnovention.co.uk>
2022-07-25 21:57:45 +0500

    close btn

--- public/js/dynamic_items.js
+++ public/js/dynamic_items.js
@@ -1,9 +1,12 @@
     // Add new element
+    $(document).ready(function(){
+
+
     $(".add").click(function(){
 
         // Finding total number of elements added
         var total_element = $(".element").length;
-                    
+
         // last <div> with element class id
         var lastid = $(".element:last").attr("id");
         var split_id = lastid.split("_");
@@ -14,23 +17,25 @@
         if(total_element < max ){
             // Adding new div container after last occurance of element class
             $(".element:last").after("<tr class='element' id='div_"+ nextindex +"'></tr>");
-                        
+
             // Adding element to <div>
-            $("#div_" + nextindex).append('<td><div class="form-group"><input type="text" class="form-control" id="exampleInput" name="title[]"  placeholder="Description"></div></td><td><div class="form-group"><textarea  class="form-control" id="exampleInput" name="description[]"  placeholder="Description"></textarea></div></td><td><div class="form-group"><input type="number" class="form-control" id="exampleInput" name="qty[]"  placeholder="Quatntity"></div></td><td><div class="form-group"><input type="text" class="form-control" id="exampleInput" name="rate[]"  placeholder="Rate"></div></td><td><div class="form-group"><input type="text" class="form-control" id="exampleInput" name="tax[]"  placeholder="Tax"></div></td><td><div id="amount_'+ nextindex +'"></div></td><td><span id="remove_' + nextindex + '" class="remove"><i class="fa fa-uncheck" aria-hidden="true"></i>X</span></td>');     
+            $("#div_" + nextindex).append('<td><div class="form-group"><input type="text" class="form-control" id="exampleInput" name="title[]"  placeholder="Description"></div></td><td><div class="form-group"><textarea  class="form-control" id="exampleInput" name="description[]"  placeholder="Description"></textarea></div></td><td><div class="form-group"><input type="number" class="form-control" id="exampleInput" name="qty[]"  placeholder="Quatntity"></div></td><td><div class="form-group"><input type="text" class="form-control" id="exampleInput" name="rate[]"  placeholder="Rate"></div></td><td><div class="form-group"><input type="text" class="form-control" id="exampleInput" name="tax[]"  placeholder="Tax"></div></td><td><div id="amount_'+ nextindex +'"></div></td><td><span id="remove_' + nextindex + '" class="remove"><i class="fa fa-uncheck" aria-hidden="true"></i>X</span></td>');
         }
-        
+
     });
         // Remove element
         //  $('.remove').on('click','.remove',function(){
         //      alert('sdf')
         //      $(this).parents('.element').remove();
         // });
-        $('.remove').on('click','.remove',function(){
-                
+        $(document).on('click','.remove',function(){
+
             var id = this.id;
             var split_id = id.split("_");
             var deleteindex = split_id[1];
-    
+
             // Remove <div> with id
             $("#div_" + deleteindex).remove();
         });
+
+})
